ad411e3e87dea090aa8aab6d73137dc1d3f02ad0
Maja Bratseth <maja@zenior.no>
2017-07-27 10:09:10 +0200

    add todo

--- core/src/main/scala/io/fintrospect/util/ExtractionError.scala
+++ core/src/main/scala/io/fintrospect/util/ExtractionError.scala
@@ -19,6 +19,8 @@ object ExtractionError {
   }
 
   case class Invalid(param: Parameter, values: Seq[String]) extends ExtractionError {
+    // todo: not very satisfying, users should probably use case matching and construct the message
+    // also, the exception should be included, it should be up to renderers to decide whether to use it
     val reason = if (values.isEmpty) "Invalid" else s"Invalid: ${values.map("'" + _ + "'").mkString(",")}"
   }
 
